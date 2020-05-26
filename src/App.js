6c92df985b46c94d5475bc1357565b4330a3ce6b
Jay Hack <jayhack.0@gmail.com>
2020-05-26 12:41:57 -0700

    updated mirabeauty link

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
             <div className="paragraph">
               <span>
                 I'm currently the co-founder and CEO at{" "}
-                <a href="http://askmira.com">Mira</a>, building the beauty
+                <a href="https://mirabeauty.com">Mira</a>, building the beauty
                 shopping experience of the future with{" "}
                 <a href="https://www.linkedin.com/in/brandon-garcia-28555850/">
                   Brandon Garcia
